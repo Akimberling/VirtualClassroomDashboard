d307422d2cf673ed9d93bb02570f8847e4ca9c2f
Amber Lynn Kimberling <59713703+Akimberling@users.noreply.github.com>
2021-05-13 14:03:57 -0700

    User Specific layout oincluding gateway in registration and login

---
+++ DataLibrary/Models/SchoolModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    class SchoolModel
+    {
+        public int SchoolID { get; set; }
+        public string SchoolName { get; set; }
+        public string SchoolLevel { get; set; }
+        public string SchoolCity { get; set; }
+        public string SchoolState { get; set; }
+    }
+}
