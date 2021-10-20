6efd55e3e57bc0feabf8bcf72dd2ea59cb534fe9
Amber Lynn Kimberling <59713703+Akimberling@users.noreply.github.com>
2021-10-19 18:39:24 -0700

    updated the teacher interface with the ability to add and manage coursees. Next steps will be to add students to a course

--- VirtualClassroomDashboard/Controllers/HomeController.cs
+++ VirtualClassroomDashboard/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting.Internal;
 using System;
 using Grpc.Core;
 using Microsoft.AspNetCore.Authorization;
+using VirtualClassroomDashboard.DataLibrary.BusinessLogic;
 
 //HomeController
 namespace VirtualClassroomDashboard.Controllers
@@ -324,24 +325,7 @@ namespace VirtualClassroomDashboard.Controllers
                 TempData["UT"] = BasicUI["UserType"];
                 TempData["SID"] = BasicUI["SchoolID"];
 
-                var schoolID = int.Parse(BasicUI["SchoolID"]);
-
-                List<UserModel> Students = new List<UserModel>();
-                var userData = UserProcessor.RetrieveNecessaryUsers(schoolID, "Student");
-
-                foreach (var row in userData)
-                {
-                    Students.Add(new UserModel
-                    {
-                        UserID = row.UserID,
-                        FirstName = row.UserFname,
-                        LastName = row.UserLname,
-                        EmailAddress = row.UserEmail,
-                        PhoneNumber = row.UserPhonNum,
-
-                    });
-
-                }
+               
                 return View();
             }
         }
@@ -629,8 +613,209 @@ namespace VirtualClassroomDashboard.Controllers
                 return View();
             }
         }
+        public IActionResult TeacherZoom()
+        {
+            //establish a dictionary that will contain user information that was set during login
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                //retrieve the necessary users and display them
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult AddCourse()
+        {
+            //establish a dictionary that will contain user information that was set during login
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                TempData["EM"] = BasicUI["EmailAddress"];
+                return View();
+            }
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult AddCourse(CourseModel model)
+        {
+
+            ViewBag.Error = "";
+            //establish a dictionary that will contain user information that was set during login
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                //save the data
+                model.UserID = int.Parse(BasicUI["UserID"]);
+                model.SchoolID = int.Parse(BasicUI["SchoolID"]);
+
+                //check to ensure there are no dulicate courses
+                List<int> uExists = CourseProcessor.CheckForDuplicates( model.CourseName, model.UserID, model.SchoolID);
+
+                if (uExists[0] != 0)
+                {
+                    ViewBag.Error = "This course already exists, please try adding a different course or speak to the admin.";
+                }
+                else
+                {
+                    //Add the course information to the database
+                    CourseProcessor.CreateCourse(model.SchoolID, model.UserID, model.CourseName, model.CourseSection, model.CourseNumber, model.ClassNum);
+                    ViewBag.Error = "The course has been added. Please refer to the view courses page to view your courses.";
+                }
+                
+                return View();
+            }
+        }
         [HttpGet]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ViewCourses()
+        {
+            //establish a dictionary that will contain user information that was set during login
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            int userId = int.Parse(BasicUI["UserID"]);
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                List<CourseModel> Courses = new List<CourseModel>();
+                var courseData = CourseProcessor.RetrieveNecessaryCourses(userId);
+
+                foreach (var row in courseData)
+                {
+                    Courses.Add(new CourseModel
+                    { 
+                        CourseID = row.CourseID,
+                        CourseName = row.CourseName,
+                        CourseSection = row.CourseSection,
+                        CourseNumber = row.CourseNumber,
+                        ClassNum = row.ClassNum
+
+                    });
+
+                }
+
+                return View(Courses);
+            }
+        }
+        public ActionResult DeleteCourse(int id)
+        {
+            CourseProcessor.deleteCourseData(id);
+
+            //check if the course exists
+            List<int> cExists = CourseProcessor.CheckForDupByID(id);
+
+            if (cExists[0] == 1)
+            {
+                ViewBag.Error = "Something went wrong.";
+            }
+            else
+            {
+                ViewBag.Error = "Course removed successfully.";
+            }
+
+            return View("ViewCourses");
+
+
+        }
+        [HttpGet]
+        public IActionResult EditCourse(int id)
+        {
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            int userId = int.Parse(BasicUI["UserID"]);
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                List<CourseModel> Courses = new List<CourseModel>();
+                var courseData = CourseProcessor.RetrieveCourse(id);
+                foreach (var row in courseData)
+                {
+                    Courses.Add(new CourseModel
+                    {
+                        CourseID = row.CourseID,
+                        CourseName = row.CourseName,
+                        CourseSection = row.CourseSection,
+                        CourseNumber = row.CourseNumber,
+                        ClassNum = row.ClassNum
+
+                    });
+                }
+                //create a new model
+                CourseModel CurrentInfo = new CourseModel();
+
+                //store the values from userData into the model
+                CurrentInfo.CourseID = Courses[0].CourseID;
+                CurrentInfo.CourseName = Courses[0].CourseName;
+                CurrentInfo.CourseSection = Courses[0].CourseSection;
+                CurrentInfo.CourseNumber = Courses[0].CourseNumber;
+                CurrentInfo.ClassNum = Courses[0].CourseNumber;
+
+                CourseInfoClass.setCourseData(CurrentInfo);
+                TempData["CName"] =Courses[0].CourseName;
+                TempData["CN"] = Courses[0].CourseNumber;
+                TempData["ClN"] = Courses[0].ClassNum;
+                TempData["CSec"] = Courses[0].CourseSection;
+
+                return View();
+            }
+        }
+        [HttpPost]
+        public IActionResult EditCourse(CourseUpdateModel model)
+        {
+            Dictionary<string, string> BasicUI = new Dictionary<string, string>();
+            BasicUI = UserInfoClass.getUserData();
+            //form of authentication
+            if (BasicUI["UserType"] != "Teacher")
+            {
+
+                return View("AccessDenied");
+
+            }
+            else
+            {
+                Dictionary<string, string> BasicCI = new Dictionary<string, string>();
+                BasicCI = CourseInfoClass.getCourseData();
+
+                CourseProcessor.updateCourseInfo(int.Parse(BasicCI["CourseID"]), model.CourseSection, model.CourseName);
+
+                return View("ViewCourses");
+            }
+        }
         public IActionResult AccessDenied()
         {
             //establish a dictionary that will contain user information that was set during login
@@ -639,6 +824,7 @@ namespace VirtualClassroomDashboard.Controllers
             TempData["UT"] = BasicUI["UserType"];
             return View();
         }
+        [HttpGet]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ VirtualClassroomDashboard/DataLibrary/BusinessLogic/CourseProcessor.cs
@@ -0,0 +1,95 @@
+﻿using VirtualClassroomDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VirtualClassroomDashboard.DataAccess;
+using VirtualClassroomDashboard.DataLibrary.Models;
+
+namespace VirtualClassroomDashboard.DataLibrary.BusinessLogic
+{
+    public class CourseProcessor
+    {
+        public static int CreateCourse(int sID, int uID, string courseNam, string courseS, string courseNum, string clNum)
+        {
+            CourseModelData data = new CourseModelData {
+                CourseID = 0,
+                SchoolID = sID,
+                UserID = uID,
+                CourseName = courseNam,
+                CourseSection = courseS,
+                CourseNumber = courseNum,
+                ClassNum = clNum
+
+            };
+
+            string sql = @"INSERT INTO dbo.COURSES (CourseID, CourseName, CourseSection, CourseNumber, ClassNum, UserID, SchoolID) VALUES (@CourseID, @CourseName, @CourseSection, @CourseNumber, @ClassNum, @UserID, @SchoolID);";
+
+            return sqlDataAccess.SaveData(sql, data);
+
+        }
+        public static List<int> CheckForDuplicates(string CourseName, int UID, int SID)
+        {
+            string sql = "SELECT COUNT(*) FROM dbo.COURSES WHERE CourseName = \'" + CourseName + "\' AND UserID = \'" + UID + "\' AND SchoolID = \'" + SID + "\';";
+
+            return sqlDataAccess.LoadData<int>(sql);
+        }
+        public static List<int> CheckForDupByID(int CourseID)
+        {
+            string sql = "SELECT COUNT(*) FROM dbo.COURSES WHERE CourseID = \'" + CourseID + "\';";
+
+            return sqlDataAccess.LoadData<int>(sql);
+        }
+        public static int deleteCourseData(int courseID)
+        {
+            CourseModelData data = new CourseModelData
+            {
+                CourseID = courseID
+            };
+            string sql = "DELETE FROM dbo.COURSES WHERE CourseID = @CourseID;";
+
+            return sqlDataAccess.SaveData(sql, data);
+        }
+        public static int updateCourseInfo(int courseID, string courseS, string courseNam)
+        {
+            CourseModelData data = new CourseModelData
+            {
+                CourseID = courseID,
+                CourseName = courseNam,
+                CourseSection = courseS
+            };
+            if(courseNam == null)
+            {
+                string sql = "UPDATE dbo.COURSES SET CourseSection = @CourseSection WHERE CourseID = @CourseID;";
+                return sqlDataAccess.SaveData(sql, data);
+            }
+            else if (courseS == null)
+            {
+                string sql = "UPDATE dbo.COURSES SET CourseName = @CourseName WHERE CourseID = @CourseID;";
+                return sqlDataAccess.SaveData(sql, data);
+            }
+            else
+            {
+                string sql = "UPDATE dbo.COURSES SET CourseName = @CourseName, CourseSection = @CourseSection WHERE CourseID = @CourseID;";
+                return sqlDataAccess.SaveData(sql, data);
+            }
+        }
+        public static List<CourseModelData> RetrieveNecessaryCourses(int UID)
+        {
+            string sql = "SELECT * FROM dbo.COURSES WHERE UserID = \'" + UID + "\';";
+
+            return sqlDataAccess.LoadData<CourseModelData>(sql);
+        }
+        public static List<CourseModelData> RetrieveCourse(int courseId)
+        {
+            string sql = "SELECT * FROM dbo.COURSES WHERE CourseID = \'" + courseId + "\';";
+
+            return sqlDataAccess.LoadData<CourseModelData>(sql);
+        }
+        public static List<int> getCourseData(int userID, string cName)
+        {
+            string sql = "SELECT CourseID FROM dbo.COURSES WHERE UserID = \'" + userID + "\' CourseName = \'" + cName + "\';";
+
+            return sqlDataAccess.LoadData<int>(sql);
+        }
+    }
+}
